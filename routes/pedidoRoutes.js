15229c3dddb6c433cec447a01faabd9f29c3d265
Jorge310506 <149962378+Jorge310506@users.noreply.github.com>
2026-05-22 13:11:02 -0400

    Add pedido routes for CRUD operations

---
+++ routes/pedidoRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const pedidoController = require('../controllers/pedidoController');
+
+router.post('/', pedidoController.createPedido);
+router.get('/', pedidoController.getAllPedidos);
+router.get('/:id', pedidoController.getPedidoById);
+router.put('/:id', pedidoController.updatePedido);
+router.delete('/:id', pedidoController.deletePedido);
+
+module.exports = router;
